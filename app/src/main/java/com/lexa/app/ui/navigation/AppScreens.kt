103399c2cada1c6b50a19363d120169a814cf407
Luis Eduardo <eduardohdz2111@gmail.com>
2025-11-04 00:02:27 -0600

    feat: Agrega iconos vectoriales y define AppScreens para navegacion

---
+++ app/src/main/java/com/lexa/app/ui/navigation/AppScreens.kt
@@ -0,0 +1,39 @@
+package com.lexa.app.ui.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.lexa.app.R
+
+// Contendra la definicion de cada una de nuestras pantallas
+sealed class AppScreen (
+    val route: String,
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int
+){
+    // Definimos la pantalla de Chat
+    data object Chat : AppScreen(
+        route = "chat",
+        title = R.string.screen_title_chat,
+        icon = R.drawable.ic_chat
+    )
+
+    // Definimos la pantalla de Abogados
+    data object Lawyers : AppScreen(
+        route = "lawyers",
+        title = R.string.screen_title_lawyers,
+        icon = R.drawable.ic_lawyers
+    )
+
+    // Definimos la pantalla de Foro
+    data object Forum : AppScreen(
+        route = "forum",
+        title = R.string.screen_title_forum,
+        icon = R.drawable.ic_forum
+    )
+}
+
+val bottomNavScreens = listOf(
+    AppScreen.Chat,
+    AppScreen.Lawyers,
+    AppScreen.Forum,
+)
